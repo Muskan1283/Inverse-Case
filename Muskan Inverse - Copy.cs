d76dfa0da097f787202d5a2f220f60126bd51319
Muskan Sagar <99795889+Muskan1283@users.noreply.github.com>
2022-04-06 13:23:34 +0530

    Add files via upload

---
+++ Muskan Inverse - Copy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "raj kumar sharma";
+        string[] strArr = str.ToUpper().Split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strArr.Length; i++)
+        {
+            var str1 = strArr[i].ToLower();
+            String str2 = strArr[i].Replace(strArr[i][0], str1[0]);
+            sb.Append(str2 + "  ");
+
+        }
+        Console.WriteLine(sb);
+    }
+}
